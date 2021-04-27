6edd0c3af45d5507b22fe35819793f7767149f56
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-27 19:56:27 +0900

    while

---
+++ loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Loop</title>
+  </head>
+  <body>
+    <h1>while</h1>
+    <?php
+    $i = 0;
+    while($i < 3){
+      echo $i."<br>";
+      $i = $i + 1;
+    }
+    ?>
+  </body>
+</html>
