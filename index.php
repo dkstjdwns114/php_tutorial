13a4b0f070501991a983106efcbb9ad17ab58536
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-27 19:57:00 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+      <li><a href="index.php?id=HTML">HTML</a></li>
+      <li><a href="index.php?id=CSS">CSS</a></li>
+      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+      <li><a href="index.php?id=PHP">PHP</a></li>
+    </ol>
+    <h2>
+      <?php
+      if(isset($_GET['id'])){
+        echo $_GET['id'];
+      }else{
+        echo "Welcome";
+      }
+      ?>
+    </h2>
+    <?php
+    if(isset($_GET['id'])){
+      $contents = file_get_contents("data/".$_GET['id']);
+      echo $contents;
+    }else{
+      echo "Hello, PHP";
+    }
+    ?>
+  </body>
+</html>
