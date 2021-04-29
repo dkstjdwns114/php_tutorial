2d25a546a410129ddb701b3b890eef858f49c8e5
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 19:02:22 +0900

    ymsqli_error

--- insert.php
+++ insert.php
@@ -1,11 +1,15 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "1234", "opentutorials");
-mysqli_query($conn, "
-  INSERT INTO topic (title, description, created)
+$sql = "
+  INSER INTO topic (title, description, created)
   VALUE(
     'MySQL',
     'MySQL is ...',
     NOW()
   )
-");
+";
+$result = mysqli_query($conn, $sql);
+if($result === false){
+  echo mysqli_error($conn);
+}
 ?>
