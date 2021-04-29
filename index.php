0696dc5f24cef8d448824b9f726b5d00e9cb4a03
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 18:31:34 +0900

    php mysql setup

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
-<?php
-require_once('./lib/print.php');
-require_once('./view/top.php');
-?>
-  <a href="create.php">create</a>
-
-  <?php if(isset($_GET['id'])){ ?>
-    <a href="update.php?id=<?= $_GET['id']; ?>">update</a>
-    <form action="delete_process.php" method="post">
-      <input type="hidden" name="id" value="<?= $_GET['id']?>">
-      <input type="submit" value="delete" >
-    </form>
-  <?php } ?>
-
-  <h2>
-    <?php
-    print_title();
-    ?>
-  </h2>
-  <?php
-  print_description();
-  ?>
-<?php
-require_once('./view/bottom.php');
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WEB</title>
+  </head>
+  <body>
+    <h1>WEB</h1>
+    <ol>
+      <li>HTML</li>
+    </ol>
+    <h2>Welcome</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </body>
+</html>
