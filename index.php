5e1c7aa39fd4b42a0111d1e3ad190b5d257dfc3a
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 21:04:27 +0900

    CRUD 완료

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $article = array(
   'description' => 'Hello, web'
 );
 $update_link = '';
+$delete_link = '';
 if(isset($_GET['id'])){
   $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
   $sql = "SELECT * FROM topic WHERE id={$filtered_id}";
@@ -23,6 +24,11 @@ if(isset($_GET['id'])){
   $article['description'] = htmlspecialchars($row['description']);
 
   $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+  $delete_link = '
+    <form action="process_delete.php" method="post">
+      <input type="hidden" name="id" value="'.$_GET['id'].'">
+      <input type="submit" value="delete">
+    </form>';
 }
 ?>
 <!DOCTYPE html>
@@ -38,6 +44,7 @@ if(isset($_GET['id'])){
     </ol>
     <a href="create.php">create</a>
     <?= $update_link ?>
+    <?= $delete_link ?>
     <h2><?= $article['title'] ?></h2>
     <?= $article['description'] ?>
   </body>
