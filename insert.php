8274bf703e642fe5c35e790b4c7f2937ba7215e0
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 18:55:46 +0900

    INSERT

--- insert.php
+++ insert.php
@@ -1,3 +1,11 @@
 <?php
-mysqli_connect("localhost", "root", "1234", "opentutorials");
+$conn = mysqli_connect("localhost", "root", "1234", "opentutorials");
+mysqli_query($conn, "
+  INSERT INTO topic (title, description, created)
+  VALUE(
+    'MySQL',
+    'MySQL is ...',
+    NOW()
+  )
+");
 ?>
