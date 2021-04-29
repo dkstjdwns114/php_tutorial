75b8f0001d8e564bd289be6a295c1b36dec804fb
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 12:14:09 +0900

    Create

--- create.php
+++ create.php
@@ -44,13 +44,14 @@ function print_list(){
       ?>
     </ol>
     <a href="create.php">create</a>
-    <h2>
-      <?php
-      print_title();
-      ?>
-    </h2>
-    <?php
-    print_description();
-    ?>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" rows="3" cols="22" placeholder="Description"></textarea>
+      </p>
+      <input type="submit">
+    </form>
   </body>
 </html>
