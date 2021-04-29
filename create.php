00a5b1852fd19135acaba6394dd3f29c95363f37
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 19:49:15 +0900

    Read 완료

--- create.php
+++ create.php
@@ -1,3 +1,25 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "1234", "opentutorials");
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)){
+  $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+
+$article = array(
+  'title' => 'Welcome',
+  'description' => 'Hello, web'
+);
+if(isset($_GET['id'])){
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $article['title'] = $row['title'];
+  $article['description'] = $row['description'];
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,9 +27,9 @@
     <title>WEB</title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
-      <li>HTML</li>
+      <?= $list ?>
     </ol>
     <form action="process_create.php" method="post">
       <p><input type="text" name="title" placeholder="Title"></p>
--- index.php
+++ index.php
@@ -7,6 +7,18 @@ $list = '';
 while($row = mysqli_fetch_array($result)){
   $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
 }
+
+$article = array(
+  'title' => 'Welcome',
+  'description' => 'Hello, web'
+);
+if(isset($_GET['id'])){
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $article['title'] = $row['title'];
+  $article['description'] = $row['description'];
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,12 +27,12 @@ while($row = mysqli_fetch_array($result)){
     <title>WEB</title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
       <?= $list ?>
     </ol>
     <a href="create.php">create</a>
-    <h2>Welcome</h2>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <h2><?= $article['title'] ?></h2>
+    <?= $article['description'] ?>
   </body>
 </html>
