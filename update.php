db8e87a5e4c9057a0996d9ebab84eca75c4e355d
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-29 12:31:58 +0900

    Update

--- update.php
+++ update.php
@@ -43,19 +43,15 @@ function print_list(){
       print_list();
       ?>
     </ol>
-    <a href="create.php">create</a>
-
-    <?php if(isset($_GET['id'])){ ?>
-      <a href="update.php?id=<?= $_GET['id']; ?>">update</a>
-    <?php } ?>
-
-    <h2>
-      <?php
-      print_title();
-      ?>
-    </h2>
-    <?php
-    print_description();
-    ?>
+    <form action="update_process.php" method="post">
+      <input type="hidden" name="old_title" value=<?= $_GET['id'] ?>>
+      <p>
+        <input type="text" name="title" placeholder="Title" value=<?= print_title() ?>>
+      </p>
+      <p>
+        <textarea name="description" rows="8" cols="40" placeholder="Description"><?= print_description() ?></textarea>
+      </p>
+      <input type="submit">
+    </form>
   </body>
 </html>
